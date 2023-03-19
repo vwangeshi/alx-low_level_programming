cca4b87a17611a386ec511267c9611f8819727ad
vwangeshi <wangeshikariuki18@gmail.com>
2023-03-19 14:36:25 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - the main function
- *
+/**
+ * Description: main - print a string with a new line
  * Return: always 0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
