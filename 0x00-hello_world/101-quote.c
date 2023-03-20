311a3bf8d5e4def4f7460ff43e8e0f8bc6f4ade3
vwangeshi <wangeshikariuki18@gmail.com>
2023-03-20 06:42:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
+
