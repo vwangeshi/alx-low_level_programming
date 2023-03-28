aad35a818a589775ec91f78157a81a349dc95dae
vwangeshi <wangeshikariuki18@gmail.com>
2023-03-28 13:43:01 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - shows the a and  b are pointers
+ * @a: the value of a
+ * @b: the value of b
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
