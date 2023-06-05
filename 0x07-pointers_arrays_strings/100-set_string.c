665785d978ae2816163a37bd39ffffb30968f4cc
vwangeshi <valwangeci@gmail.com>
2023-06-05 16:05:17 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_string - entry  point
+ * @s: pointer to a  pointer
+ * @to: pointer char
+ *
+ * Return: 0
+ */
+
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
